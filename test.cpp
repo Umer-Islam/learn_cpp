c3ef105af98f857ce5c753504e8ae3efe7021d23
umer islam <139012668+Umer-Islam@users.noreply.github.com>
2024-06-30 21:07:52 +0500

    initial

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    cout << "...hello world..." << endl;
+
+    int a;
+    a = 34;
+    cout << "size of a: " << sizeof(a);
+
+
+    float b ;
+    b = 45.454545454545469876987698776;
+    cout << "\n size of b:" << sizeof(b);
+    return 0;
+
+}
